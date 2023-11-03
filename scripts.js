6dacc62e6461d46c7ac9c4a81a5d7a5af9e388ee
marcosdeaquino <marcos.deaquino2018@gmail.com>
2023-11-03 17:36:30 -0300

    codigo alterado

--- scripts.js
+++ scripts.js
@@ -1,9 +1,13 @@
-function generateNumber() {
+function generateNumber(){
 
  const min = Math.ceil(document.querySelector('.input-min').value)
  const max = Math.floor(document.querySelector('.input-max').value)
 
- const result = Math.floor(Math.rondom() * (max - min + 1)) + min;
-
- alert(result)
+ if(min >= max) {
+    alert('o valor minimo tem que ser menor que o valor maximo')
+ } else {
+    const result = Math.floor(Math.random() * (max - min + 1)) + min;
+    
+    alert(result)
+ }
 }
