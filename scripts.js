6be409285cbadf013a260975b873b8b68e2abae6
marcosdeaquino <marcos.deaquino2018@gmail.com>
2023-11-03 17:57:42 -0300

    adição de novos arquivos

--- scripts.js
+++ scripts.js
@@ -1,4 +1,4 @@
-function generateNumber(){
+function generateNumber() {
 
  const min = Math.ceil(document.querySelector('.input-min').value)
  const max = Math.floor(document.querySelector('.input-max').value)
@@ -6,8 +6,8 @@ function generateNumber(){
  if(min >= max) {
     alert('o valor minimo tem que ser menor que o valor maximo')
  } else {
-    const result = Math.floor(Math.random() * (max - min + 1)) + min;
+    const result = Math.floor(Math.random() * (max - min + 1) + min)
     
-    alert(result)
+    console.log(result)
  }
 }
